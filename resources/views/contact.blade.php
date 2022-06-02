27f209ffdc6bd4680924fba6128c0607fab10e68
Sumesh Ramdin <sumeshramdin@gmail.com>
2022-06-02 12:20:01 +0000

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,2 @@
+<h1>This is the contact page</h1>
+<a href="{{ url('/about') }}">About page</a>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Demo\DemoContoller;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(DemoContoller::class)->group(function () {
+    Route::get('/about', 'index');
+    Route::get('/contact', 'contactMethod');
 });
